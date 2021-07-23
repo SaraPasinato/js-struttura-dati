8104705acf4da0f5e675f90ff8a35bd9ee9d6b7f
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-23 15:02:41 +0200

    clean code + set scripts

--- js/script.js
+++ js/script.js
@@ -58,71 +58,184 @@ const card = {
 console.group("debug inline:");
 console.debug(card);
 
-
-
+//?element html vars
 const cardItems = document.getElementById("card-items");
-
 
 //! stampo card in html la variabile card
 
-//? custruction string subType
-const subType = card.subType ? `- ${card.subType}` : ``;
-//? custruction string cite
-const cite= card.flavorText.author ? `</br> - ${card.flavorText.author}`: ``;
-//? custruction descriptrions string 
-let abilitiesContent=`<em> Nessuna Abilità </em>`;
-if(card.abilities.length){
-    abilitiesContent=`<ul>`;
-    for (let i=0; i<card.abilities.length;i++){
-       const currentItem= card.abilities[i];
-       abilitiesContent+= `</br>`
-       abilitiesContent+= `<li> ${i+1}. <strong>Descrizione:</strong></br> ${currentItem.description}</li>`
-       abilitiesContent+= `<li>Costo di Lancio:</br>  ${currentItem.launchCost.join(", ")}</li>`
-    }
-    abilitiesContent+=`</ul>`;
+
+/** function arrow to render Card  and inject in html element 
+ * 
+ * @param {Object} obj  card
+ * @returns {string} cardTamplate 
+ */
+const renderCard = (obj) =>{
+ //? custruction string subType
+ const subType = obj.subType ? `- ${obj.subType}` : ``;
+ //? custruction string cite
+ const cite = obj.flavorText.author ? `</br> - ${obj.flavorText.author}` : ``;
+ //? custruction descriptrions string 
+ let abilitiesContent = `<em> Nessuna Abilità </em>`;
+ if (obj.abilities.length) {
+     abilitiesContent = `<ul>`;
+     for (let i = 0; i < obj.abilities.length; i++) {
+         const currentItem = obj.abilities[i];
+         abilitiesContent += `</br>`
+         abilitiesContent += `<li> ${i + 1}. <strong>Descrizione:</strong></br> ${currentItem.description}</li>`
+         abilitiesContent += `<li>Costo di Lancio:</br>  ${currentItem.launchCost.join(", ")}</li>`
+     }
+     abilitiesContent += `</ul>`;
+ }
+ // ! string tamplate text html
+ let cardTemplate = `
+<ul class="card-info">
+     <li><strong>Id:</strong> ${obj.id}</li> 
+     <li><strong>Nome:</strong> ${obj.name}</li>   
+     <li><strong>Costo</strong> d'attivazione: ${obj.cost}</li> 
+     <li><strong>CmC:</strong> ${obj.cmc}</li>     
+     <li><strong>Tipo carta:</strong> ${obj.cardType} ${subType}</li>     
+     <li><strong> Espansione:</strong>
+         <ul>
+         <li>Ristampa: ${obj.expansion.reprintId}</li>
+         <li>Nome: ${obj.expansion.name}</li>
+         <li>Rarità: ${obj.expansion.rarity}</li>
+         <li>Numero di Collezione: ${obj.expansion.collectionNr}/${obj.expansion.totalCard}</li>
+         </ul>
+     </li>
+     <li><strong>Testo di colore:</strong>
+     <ul>
+         <li>${obj.flavorText.quote} ${cite}</li>
+     </ul>
+     </li>
+     <li> <strong> Abilità:</strong> ${abilitiesContent}</li>
+     <li> <strong>Costituzione:</strong> ${obj.costitution}</li>
+     <li> <strong>Forza:</strong> ${obj.strength}</li>
+     <li> <strong>Colore Bordo:</strong> ${obj.borderColor}</li>
+     <li><strong> Illustratore:</strong>
+     <ul>
+         <li><em>Nome:</em> ${obj.illustrator.author.name}</li>
+         <li><em>Link immagine:</em> ${obj.illustrator.source}</li> 
+     </ul>
+ </li>
+     <li><strong> Background:</strong>
+     <ul>
+         <li><em>Colore Sfondo:</em> ${obj.background.color}</li>
+         <li><em>Link Sfondo:</em> ${obj.background.source}</li> 
+     </ul>
+ </li>
+</ul>`;
+
+ //? return strin
+return cardTemplate;
 }
 
-
-// ! string tamplate text html
-let cardTemplate = `
-<ul class="card-info">
-    <li><strong>Id:</strong> ${card.id}</li> 
-    <li><strong>Nome:</strong> ${card.name}</li>   
-    <li><strong>Costo</strong> d'attivazione: ${card.cost}</li> 
-    <li><strong>CmC:</strong> ${card.cmc}</li>     
-    <li><strong>Tipo carta:</strong> ${card.cardType} ${subType}</li>     
-    <li><strong> Espansione:</strong>
+let tamplate=renderCard(card);
+cardItems.innerHTML=tamplate;
+ 
+/**
+ * Ex function renderCard
+ */
+/*function renderCard(obj) {
+    //? custruction string subType
+    const subType = obj.subType ? `- ${obj.subType}` : ``;
+    //? custruction string cite
+    const cite = obj.flavorText.author ? `</br> - ${obj.flavorText.author}` : ``;
+    //? custruction descriptrions string 
+    let abilitiesContent = `<em> Nessuna Abilità </em>`;
+    if (obj.abilities.length) {
+        abilitiesContent = `<ul>`;
+        for (let i = 0; i < obj.abilities.length; i++) {
+            const currentItem = obj.abilities[i];
+            abilitiesContent += `</br>`
+            abilitiesContent += `<li> ${i + 1}. <strong>Descrizione:</strong></br> ${currentItem.description}</li>`
+            abilitiesContent += `<li>Costo di Lancio:</br>  ${currentItem.launchCost.join(", ")}</li>`
+        }
+        abilitiesContent += `</ul>`;
+    }
+    // ! string tamplate text html
+    let cardTemplate = `
+   <ul class="card-info">
+        <li><strong>Id:</strong> ${obj.id}</li> 
+        <li><strong>Nome:</strong> ${obj.name}</li>   
+        <li><strong>Costo</strong> d'attivazione: ${obj.cost}</li> 
+        <li><strong>CmC:</strong> ${obj.cmc}</li>     
+        <li><strong>Tipo carta:</strong> ${obj.cardType} ${subType}</li>     
+        <li><strong> Espansione:</strong>
+            <ul>
+            <li>Ristampa: ${obj.expansion.reprintId}</li>
+            <li>Nome: ${obj.expansion.name}</li>
+            <li>Rarità: ${obj.expansion.rarity}</li>
+            <li>Numero di Collezione: ${obj.expansion.collectionNr}/${obj.expansion.totalCard}</li>
+            </ul>
+        </li>
+        <li><strong>Testo di colore:</strong>
         <ul>
-        <li>Ristampa: ${card.expansion.reprintId}</li>
-        <li>Nome: ${card.expansion.name}</li>
-        <li>Rarità: ${card.expansion.rarity}</li>
-        <li>Numero di Collezione: ${card.expansion.collectionNr}/${card.expansion.totalCard}</li>
+            <li>${obj.flavorText.quote} ${cite}</li>
+        </ul>
+        </li>
+        <li> <strong> Abilità:</strong> ${abilitiesContent}</li>
+        <li> <strong>Costituzione:</strong> ${obj.costitution}</li>
+        <li> <strong>Forza:</strong> ${obj.strength}</li>
+        <li> <strong>Colore Bordo:</strong> ${obj.borderColor}</li>
+        <li><strong> Illustratore:</strong>
+        <ul>
+            <li><em>Nome:</em> ${obj.illustrator.author.name}</li>
+            <li><em>Link immagine:</em> ${obj.illustrator.source}</li> 
         </ul>
     </li>
-    <li><strong>Testo di colore:</strong>
-       <ul>
-          <li>${card.flavorText.quote} ${cite}</li>
-       </ul>
+        <li><strong> Background:</strong>
+        <ul>
+            <li><em>Colore Sfondo:</em> ${obj.background.color}</li>
+            <li><em>Link Sfondo:</em> ${obj.background.source}</li> 
+        </ul>
     </li>
-    <li> <strong> Abilità:</strong> ${abilitiesContent}</li>
-    <li> <strong>Costituzione:</strong> ${card.costitution}</li>
-    <li> <strong>Forza:</strong> ${card.strength}</li>
-    <li> <strong>Colore Bordo:</strong> ${card.borderColor}</li>
-    <li><strong> Illustratore:</strong>
-     <ul>
-        <li><em>Nome:</em> ${card.illustrator.author.name}</li>
-        <li><em>Link immagine:</em> ${card.illustrator.source}</li> 
-     </ul>
-   </li>
-    <li><strong> Background:</strong>
-     <ul>
-        <li><em>Colore Sfondo:</em> ${card.background.color}</li>
-        <li><em>Link Sfondo:</em> ${card.background.source}</li> 
-     </ul>
-   </li>
-</ul>
-`;
+   </ul>`;
+
+    //? return strin
+   return cardTemplate;
+}  */
 
 
-//? puts string in html element
-cardItems.innerHTML = cardTemplate;
+//!STUB: function  renderCard general
+/* 
+function renderCard(obj, element){
+    // get property obj
+      let properties= Object.keys(obj);
+      let cardTemplate=`<ul class="card-info">`
+      console.log(properties);
+      //if exists property
+      if(properties.length){
+          //iteration on propreties elements
+        for(let i=0; i<properties.length; i++){
+            //get property 1level
+            let currentProp= properties[i];
+          
+            console.log(currentProp, typeof obj[currentProp]);
+           
+          if (typeof obj[currentProp] === 'object' ){
+            cardTemplate+= `<li> ${currentProp}:</li>`; 
+                //get property 2level
+              let tmpObj=obj[currentProp];
+               let subPropeties=Object.keys(tmpObj);
+               console.log("sub:"+subPropeties + "legth: "+subPropeties.length);
+               cardTemplate+=`<ul>`;
+                for(let j=0; j<subPropeties.length; j++){
+                    console.log(subPropeties[j] + ": " + obj[currentProp.subPropeties]);
+                    cardTemplate+=`<li><em> ${subPropeties[j]}</em>: ${tmpObj[subPropeties[j]]}</li>`;
+                } 
+                cardTemplate+=`</ul>`;
+             }else{
+                cardTemplate+= `<li> ${currentProp}: ${obj[currentProp]}</li>`; 
+             }
+            
+           
+        }
+      }
+    
+      cardTemplate+=`</ul>`;
+    
+      console.log(cardTemplate);
+      element.innerHTML=cardTemplate;
+    } */
+    
+    
