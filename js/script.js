0da837d6e247df668ccaa8074dc0e52d4ea14e15
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-23 16:13:03 +0200

    add render Deck

--- js/script.js
+++ js/script.js
@@ -57,12 +57,57 @@ const card = {
         source: "img/pic.jpg",
     },
 };
+
 //? debug
 console.group("debug card:");
 console.debug(card);
 console.groupEnd();
 //! inizializzo l'array deck
-let initialDeck = [card,
+let initialDeck = [
+    {
+        id: 1,
+        name: "Bloodfire Colossus",
+        cost: ['6', 'R', 'R'],
+        cmc: 8,
+        cardType: "creature",
+        subType: "giant",
+        expansion: {
+            reprintId: 9,
+            name: "Apocalypse",
+            rarity: "GoldenRod",
+            collectionNr: 55,
+            totalCard: 143,
+        },
+        flavorText: {
+            quote: "took all its strength to contain the fire within.",
+            author: "Greg Staples",
+        },
+        abilities: [
+            {
+                launchCost: ['R', 'T'],
+                description: "Sacrifice Bloodfire Colossus: It deals 6 damage to each creature and each player.",
+            },
+            {
+                launchCost: ['R', 'R', 'R'],
+                description: "Whenever you cast a noncreature spell, surveil 1.You may put that card into your graveyard.",
+            },
+
+        ],
+        costitution: 6,
+        strength: 6,
+        borderColor: "#000",
+        illustrator: {
+            author: {
+                id: 1,
+                name: "Greg Staples",
+            },
+            source: "img/pic.jpg",
+        },
+        background: {
+            color: "red",
+            source: "img/pic.jpg",
+        },
+    },
     {
         id: 2,
         name: "Bear Umbra",
@@ -149,7 +194,7 @@ let initialDeck = [card,
 ];
 //? debug
 console.group("debug deck:");
-console.debug(InitialDeck);
+console.debug(initialDeck);
 
 
 
@@ -164,17 +209,17 @@ const cardItems = document.getElementById("card-items");
  * @param {Object} obj  card
  * @returns {string} cardTamplate 
  */
-const renderCard = (obj) => {
+const renderCard = (card) => {
     //? custruction string subType
-    const subType = obj.subType ? `- ${obj.subType}` : ``;
+    const subType = card.subType ? `- ${card.subType}` : ``;
     //? custruction string cite
-    const cite = obj.flavorText.author ? `</br> - ${obj.flavorText.author}` : ``;
+    const cite = card.flavorText.quote ? ` </br> - ${card.flavorText.author}` : ``;
     //? custruction descriptrions string 
     let abilitiesContent = `<em> Nessuna Abilità </em>`;
-    if (obj.abilities.length) {
+    if (card.abilities.length) {
         abilitiesContent = `<ul>`;
-        for (let i = 0; i < obj.abilities.length; i++) {
-            const currentItem = obj.abilities[i];
+        for (let i = 0; i < card.abilities.length; i++) {
+            const currentItem = card.abilities[i];
             abilitiesContent += `</br>`
             abilitiesContent += `<li> ${i + 1}. <strong>Descrizione:</strong></br> ${currentItem.description}</li>`
             abilitiesContent += `<li>Costo di Lancio:</br>  ${currentItem.launchCost.join(", ")}</li>`
@@ -184,38 +229,38 @@ const renderCard = (obj) => {
     // ! string tamplate text html
     let cardTemplate = `
 <ul class="card-info">
-     <li><strong>Id:</strong> ${obj.id}</li> 
-     <li><strong>Nome:</strong> ${obj.name}</li>   
-     <li><strong>Costo</strong> d'attivazione: ${obj.cost}</li> 
-     <li><strong>CmC:</strong> ${obj.cmc}</li>     
-     <li><strong>Tipo carta:</strong> ${obj.cardType} ${subType}</li>     
+     <li><strong>Id:</strong> ${card.id}</li> 
+     <li><strong>Nome:</strong> ${card.name}</li>   
+     <li><strong>Costo</strong> d'attivazione: ${card.cost}</li> 
+     <li><strong>CmC:</strong> ${card.cmc}</li>     
+     <li><strong>Tipo carta:</strong> ${card.cardType} ${subType}</li>     
      <li><strong> Espansione:</strong>
          <ul>
-         <li>Ristampa: ${obj.expansion.reprintId}</li>
-         <li>Nome: ${obj.expansion.name}</li>
-         <li>Rarità: ${obj.expansion.rarity}</li>
-         <li>Numero di Collezione: ${obj.expansion.collectionNr}/${obj.expansion.totalCard}</li>
+         <li>Ristampa: ${card.expansion.reprintId}</li>
+         <li>Nome: ${card.expansion.name}</li>
+         <li>Rarità: ${card.expansion.rarity}</li>
+         <li>Numero di Collezione: ${card.expansion.collectionNr}/${card.expansion.totalCard}</li>
          </ul>
      </li>
      <li><strong>Testo di colore:</strong>
      <ul>
-         <li>${obj.flavorText.quote} ${cite}</li>
+         <li>${card.flavorText.quote} ${cite}</li>
      </ul>
      </li>
      <li> <strong> Abilità:</strong> ${abilitiesContent}</li>
-     <li> <strong>Costituzione:</strong> ${obj.costitution}</li>
-     <li> <strong>Forza:</strong> ${obj.strength}</li>
-     <li> <strong>Colore Bordo:</strong> ${obj.borderColor}</li>
+     <li> <strong>Costituzione:</strong> ${card.costitution}</li>
+     <li> <strong>Forza:</strong> ${card.strength}</li>
+     <li> <strong>Colore Bordo:</strong> ${card.borderColor}</li>
      <li><strong> Illustratore:</strong>
      <ul>
-         <li><em>Nome:</em> ${obj.illustrator.author.name}</li>
-         <li><em>Link immagine:</em> ${obj.illustrator.source}</li> 
+         <li><em>Nome:</em> ${card.illustrator.author.name}</li>
+         <li><em>Link immagine:</em> ${card.illustrator.source}</li> 
      </ul>
  </li>
      <li><strong> Background:</strong>
      <ul>
-         <li><em>Colore Sfondo:</em> ${obj.background.color}</li>
-         <li><em>Link Sfondo:</em> ${obj.background.source}</li> 
+         <li><em>Colore Sfondo:</em> ${card.background.color}</li>
+         <li><em>Link Sfondo:</em> ${card.background.source}</li> 
      </ul>
  </li>
 </ul>`;
@@ -223,15 +268,22 @@ const renderCard = (obj) => {
     //? return strin
     return cardTemplate;
 }
-
-for(let i=0; i<initialDeck.length;i++){
-    const currentCard= inin
+//! write deck into html
+/**
+ * 
+ * @param {ObjectArray} deck  of cards
+ * @param {htmlElement} targetElement 
+ */
+let renderDeck = (deck, targetElement) => {
+    let deckTamplate = ` `;
+    for (let i = 0; i < deck.length; i++) {
+        deckTamplate += renderCard(deck[i]);
+    }
+    console.log(deckTamplate);
+    targetElement.innerHTML += deckTamplate;
 }
 
-let tamplate = renderCard(card);
-cardItems.innerHTML = tamplate;
-//! wirte  cards in deck  into html 
-
+renderDeck(initialDeck, cardItems);
 /**
  * Ex function renderCard
  */
